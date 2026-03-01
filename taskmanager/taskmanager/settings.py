cdb05cdea31742e82c238486914f233f52063833
Mirjalol <mirjalol0111@gmail.com>
2026-03-01 15:33:11 +0500

    Fix static files configuration

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
